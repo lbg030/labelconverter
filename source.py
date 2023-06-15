999ff7c7eb94bb139e40962373e6c05c3071a3b2
병권 <66514138+lbg030@users.noreply.github.com>
2023-06-15 18:55:21 +0900

    code refactoring

--- source.py
+++ source.py
@@ -1,13 +1,16 @@
 import sys
 import os
 import json
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel, QComboBox, QFileDialog, QMessageBox, QFormLayout
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QLabel,
+                             QComboBox, QFileDialog, QMessageBox, QFormLayout)
 
 from utils import labelme2yolo, labelme2hubble, yolo2labelme
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.directory = ""
+        self.class_list = []
         self.initUI()
 
     def initUI(self):
@@ -17,17 +20,9 @@ class MyApp(QWidget):
 
         # Setup combo boxes
         self.format_combo = QComboBox(self)
-        self.format_combo.addItem("-")
-        self.format_combo.addItem("Object Detection/Labelme2YOLO")
-        self.format_combo.addItem("Object Detection/Labelme2Hubble")
-        self.format_combo.addItem("Object Detection/YOLO2Labelme")
-
-        '''
-        #TODO: Add this function
-        self.format_combo.addItem("Segmentation/Labelme2Hubble")
-        self.format_combo.addItem("Segmentation/Lens2Hubble")
-        '''
-
+        self.format_combo.addItems(["-", "Object Detection/Labelme2YOLO",
+                                    "Object Detection/Labelme2Hubble", 
+                                    "Object Detection/YOLO2Labelme"])
         self.format_combo.currentIndexChanged.connect(self.set_format_type)
 
         # Setup buttons
@@ -57,13 +52,12 @@ class MyApp(QWidget):
         self.show()
 
     def select_directory(self):
-        self.directory = QFileDialog.getExistingDirectory(
-            self, "Select Directory")
+        self.directory = QFileDialog.getExistingDirectory(self, "Select Directory")
         if self.directory:
             self.btn_format.setEnabled(True)
             self.lbl_current_dir.setText(self.directory)
-            self.class_list = [name for name in os.listdir(
-                self.directory) if os.path.isdir(os.path.join(self.directory, name))]
+            self.class_list = sorted([name for name in os.listdir(self.directory)
+                                      if os.path.isdir(os.path.join(self.directory, name))])
 
     def set_format_type(self): 
         sender = self.sender()
@@ -72,58 +66,51 @@ class MyApp(QWidget):
 
     def format_label(self):
         format_type = self.lbl_format_type.text()
-
-        if format_type:  # Check if format type is selected
-            self.lbl_status.setText(f"Processing...")
-            QApplication.processEvents()
-
-            converted_files = {cls: 0 for cls in self.class_list}
-
-            if format_type in ["Labelme2YOLO", "Labelme2Hubble"]:
-                file_ext = '.json'
-                conversion_funcs = {
-                    "Labelme2YOLO": self.format_to_yolo,
-                    "Labelme2Hubble": self.format_to_hubble
-                }
-            else:  # "YOLO2Labelme"
-                file_ext = '.txt'
-                conversion_funcs = {"YOLO2Labelme": self.format_to_labelme}
-
-            self.process_files(file_ext, conversion_funcs[format_type], converted_files)
-        else:
+        if not format_type:  # If format type is not selected
             QMessageBox.warning(self, "Warning", "Please select a format type.")
+            return
+
+        self.lbl_status.setText(f"Processing...")
+        QApplication.processEvents()
+
+        converted_files = {cls: 0 for cls in self.class_list}
+
+        conversion_funcs = {
+            "Labelme2YOLO": self.format_to_yolo,
+            "Labelme2Hubble": self.format_to_hubble,
+            "YOLO2Labelme": self.format_to_labelme
+        }
+
+        file_ext = '.json' if format_type in ["Labelme2YOLO", "Labelme2Hubble"] else '.txt'
+        self.process_files(file_ext, conversion_funcs[format_type], converted_files)
 
     def process_files(self, file_ext, conversion_func, converted_files):
         for dir_name in self.class_list:
             subdir_path = os.path.join(self.directory, dir_name)
+            hubble_path = os.path.join(subdir_path, "hubble_json")
             
-            if not os.path.exists(f"{subdir_path}/hubble_json/"):
-                os.makedirs(f"{subdir_path}/hubble_json/")
+            os.makedirs(hubble_path, exist_ok=True)
+            
+            file_list = [f for f in os.listdir(subdir_path) if f.endswith(file_ext)]
 
-            
-            file_list = [f for f in os.listdir(
-                subdir_path) if f.endswith(file_ext)]
-
-            
             for file_name in file_list:
                 file_path = os.path.join(subdir_path, file_name)
                 with open(file_path, 'r') as file:
                     data = json.load(file)
 
-                conversion_func(data, file_path, self.class_list)
+                conversion_func(data, file_path)
                 converted_files[dir_name] += 1
 
-        self.lbl_status.setText(
-            f"Conversion complete. Class-wise counts: {converted_files}")
+        self.lbl_status.setText(f"Conversion complete. Class-wise counts: {converted_files}")
 
-    def format_to_yolo(self, data, file_path, class_list):
-        labelme2yolo(data, file_path, class_list)
+    def format_to_yolo(self, data, file_path):
+        labelme2yolo(data, file_path, self.class_list)
 
-    def format_to_hubble(self, data, file_path, class_list=None):
-        labelme2hubble(data, file_path, class_list)
+    def format_to_hubble(self, data, file_path):
+        labelme2hubble(data, file_path, self.class_list)
 
-    def format_to_labelme(self, data, class_list):
-        yolo2labelme(data, class_list)
+    def format_to_labelme(self, data, file_path):
+        yolo2labelme(data, file_path, self.class_list)
 
 
 if __name__ == '__main__':
